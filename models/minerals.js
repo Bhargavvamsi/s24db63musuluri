bec24aa22d33938625a05541f4897d25ec090e8e
Bhargavvamsi <musulurivamsi@gmail.com>
2024-04-15 22:51:28 -0500

    Validation added for HardnessLevel Attribute

--- models/minerals.js
+++ models/minerals.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose")
 const mineralsSchema = mongoose.Schema({
 Mineral_name: String,
 Color: String,
-Hardness_level: Number
+Hardness_level: {
+    type: Number,
+    min: [1, "Hardness_level must be at least 1"],
+    max: [10, "Hardness_level cannot be greater than 10"]
+  }
 })
 module.exports = mongoose.model("minerals", 
 mineralsSchema)
