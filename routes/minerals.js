a06c0d0557037dd80f64a2f8b5aefee744be20b0
Bhargavvamsi <musulurivamsi@gmail.com>
2024-04-15 22:39:42 -0500

    protect some endpoints

--- routes/minerals.js
+++ routes/minerals.js
@@ -10,6 +10,14 @@ module.exports = router;
 var express = require('express');
 const minerals_controlers= require('../controllers/minerals');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ res.redirect("/login");
+ }
 /* GET minerals */
 router.get('/', minerals_controlers.minerals_view_all_Page);
 /* GET detail minerals page */
@@ -17,7 +25,7 @@ router.get('/detail', minerals_controlers.minerals_view_one_Page);
 /* GET create minerals page */
 router.get('/create', minerals_controlers.minerals_create_Page);
 /* GET create update page */
-router.get('/update', minerals_controlers.minerals_update_Page);
-/* GET delete costume page */
+router.get('/update',secured, minerals_controlers.minerals_update_Page);
+/* GET delete minerals page */
 router.get('/delete', minerals_controlers.minerals_delete_Page);
 module.exports = router;
